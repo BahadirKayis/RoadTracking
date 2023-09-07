1eb54b307dd7ae6c82de22317f74112b5c0aca87
BahadirKayis <bahadir.kayis24@gmail.com>
2023-09-07 20:38:00 +0300

    pdf new page added,hilt ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.example.roadtracking"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.roadtracking"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -26,14 +26,14 @@ android {
                 arg("room.schemaLocation", "$projectDir/schemas")
             }
         }
-
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
             )
         }
     }
@@ -59,32 +59,29 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.46")
-    kapt("com.google.dagger:hilt-compiler:2.45")
-    kapt("com.google.dagger:hilt-android-compiler:2.44.2")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:dagger-compiler:2.48") // Dagger compiler
+    ksp("com.google.dagger:hilt-compiler:2.48") // Hilt compiler
 
-
-    //Splash Screen
+    // Splash Screen
     implementation("androidx.core:core-splashscreen:1.0.1")
-    //coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
 
-    //Navigate
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    //ViewModel
+    // Navigate
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
+    // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.activity:activity-ktx:1.7.2")
 
-    //Room
-    implementation("androidx.room:room-runtime:2.5.1")
-    implementation("androidx.room:room-ktx:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
-    annotationProcessor("androidx.room:room-compiler:2.5.1")
-
+    // Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
 
 //    Kapt, Kotlin koduyla Java işlemcilerini kullanmanıza olanak tanırken,
 //    KSP (Kotlin Symbol Processing) daha hızlı çalışır ve Kotlin'in dil yapılarını daha iyi anlar.
--- app/src/main/java/com/example/roadtracking/domain/converter/RoadExpensePdfConverter.kt
+++ app/src/main/java/com/example/roadtracking/domain/converter/RoadExpensePdfConverter.kt
@@ -10,7 +10,6 @@ import java.io.FileOutputStream
 import java.util.Calendar
 import javax.inject.Inject
 
-
 class RoadExpensePdfConverter @Inject constructor(private val context: Context) {
     private val paint = Paint().apply {
         textSize = TITLE_TEXT_SIZE
@@ -25,20 +24,20 @@ class RoadExpensePdfConverter @Inject constructor(private val context: Context) 
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val formattedMonth = month.toString().padStart(2, '0')
-        val pdfName = "Kazım Kayış-$formattedMonth-$year.pdf"
+        val pdfName = "-$formattedMonth-$year.pdf"
 
         val pdfDocument = PdfDocument()
         val fontMetrics = paint.fontMetrics
         val lineHeight = fontMetrics.descent - fontMetrics.ascent
         val x = pageInfo.pageWidth / 2f // Metnin ortalanacağı yatay pozisyon
         var y = START_VERTICAL_POSITION // İlk dikey pozisyon
-        val page = pdfDocument.startPage(pageInfo)
-        val canvas = page.canvas
-        canvas.drawText(title, x, y, paint)  // Başlığı PDF sayfasına çizin
+        var page = pdfDocument.startPage(pageInfo)
+        var canvas = page.canvas
+        canvas.drawText(title, x, y, paint) // Başlığı PDF sayfasına çizin
 
-        //Content
+        // Content
         paint.apply {
-            textSize = CONTENT_TEXT_SIZE;isFakeBoldText = false;textAlign = Paint.Align.LEFT
+            textSize = CONTENT_TEXT_SIZE; isFakeBoldText = false; textAlign = Paint.Align.LEFT
         }
         y += lineHeight
         for (item in road) {
@@ -46,6 +45,12 @@ class RoadExpensePdfConverter @Inject constructor(private val context: Context) 
             val company = item.company
             canvas.drawText("$date - $company", COLUMN_X, y, paint)
             y += lineHeight // Yeni bir satıra geçmek için y koordinatını güncelleyin
+            if (y + lineHeight >= PAGE_HEIGHT) {
+                pdfDocument.finishPage(page) // Mevcut sayfayı tamamlayın
+                page = pdfDocument.startPage(pageInfo) // Yeni bir sayfa başlatın
+                canvas = page.canvas // Yeni sayfanın canvas'ını alın
+                y = 50f // Y koordinatını sıfırlayın veya istediğiniz değere ayarlayın
+            }
         }
         pdfDocument.finishPage(page)
         val file = File(context.getExternalFilesDir(null), pdfName)
